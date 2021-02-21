79d1011916bad2424782064df2f0a47fd184a7d8
Hafeez <erudite@Hafeezs-MacBook-Pro.local>
2021-02-21 12:24:20 -0500

    Delete some comment and modify the code better

---
+++ PayHalfTime/Function_OverTime.py
@@ -0,0 +1,30 @@
+def computePay(hours, rate):
+    # print('In computePay', hours, rate)
+
+    hours = input('Enter Hours: ')
+    rate = input('Enter Rate: ')
+    timeHalf = float(1.5)
+
+    # Making Floating Variable
+
+    fh = float(hours)
+    fr = float(rate)
+
+    reg = fh * fr    
+
+    if fh > 30:
+        print('You work more than the required hours this week')
+        overTime = timeHalf * fr
+        print('Do you work overtime: Y/N')
+        que = input()
+        if que == 'y':
+            overTimeHour = int(input('overtime: '))
+            overTimePay = overTime * overTimeHour
+            xp = reg + overTimePay
+            print('Pay:$', xp)
+    else:
+        print('Pay ', reg)
+
+
+computePay(10, 11)
+
